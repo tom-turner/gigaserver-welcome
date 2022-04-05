32f416f1e04eabeebf544d24d8870cfb271e2636
tom-turner <tom.turner@live.co.uk>
2022-04-05 23:14:35 +0100

    k

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const fs = require('fs')
+
+
+let getFile = () => { 
+  return fs.readFileSync('setup.json', 'utf8').split('\n')
+}
+
+let parseFile = (file) => {
+  let services = []
+
+  for ( let line of file ) {
+    let split = line.split('=')
+    let name = split[0]
+    let url = split[1]
+    services.push({
+      name: name,
+      url: url
+    })
+  }
+  return services
+}
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs', { services : parseFile( getFile() ) })
 })
 
 app.listen(port, () => {
